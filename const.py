73797f03199fa09f415b01b21fee22f70c1c7060
root <kimiron518@gmail.com>
2024-10-16 23:57:31 +0900

    add const

---
+++ const.py
@@ -0,0 +1,2 @@
+coco_classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+kor_coco_classes = ['비행기', '자동차', '새', '고양이', '사슴', '강아지', '개구리', '말', '배', '트럭']
