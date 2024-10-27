b2133b52569162ce25902e23f52cb30b06882a24
root <kimiron518@gmail.com>
2024-10-28 00:28:19 +0900

    add preparing text features

--- eval.py
+++ eval.py
@@ -14,13 +14,24 @@ from transformers import AutoProcessor, AutoModel, AutoTokenizer
 from const import *
 
 
+templates = [
+    "{}의 사진.",
+    "흐릿한 {}의 사진.",
+    "종이접기 {}.",
+    "큰 {}의 사진.",
+    "비디오 게임 속 {}.",
+    "예술적인 {}.",
+    "작은 {}의 사진."
+]
+
+
 def accuracy(output, target, topk=(1,)):
     pred = output.topk(max(topk), 1, True, True)[1].t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
     return [correct[:k].reshape(-1).float().sum(0, keepdim=True).cpu().numpy().item() for k in topk]
 
 
-def eval(model, text_inputs, test_loader, device):
+def evaluation(model, text_features, test_loader, device):
     model.eval()
 
     total_correct, step = 0., 0.
@@ -29,9 +40,11 @@ def eval(model, text_inputs, test_loader, device):
         for i, (images, labels) in tqdm(enumerate(test_loader)):
             images, labels = images.to(device), labels.to(device)
 
-            output = model(pixel_values=images, **text_inputs)
+            image_features = model.get_image_features(pixel_values=images)
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            logits = 100. * image_features @ text_features 
 
-            acc1, acc5 = accuracy(output.logits_per_image, labels, topk=(1, 5))
+            acc1, acc5 = accuracy(logits, labels, topk=(1, 5))
             top1 += acc1
             top5 += acc5
             n += images.size(0)
@@ -54,7 +67,7 @@ def load_data(dataset_name, batch_size=32, image_size=224, num_workers=2):
         ])
         test_data = torchvision.datasets.CIFAR10(root="./data", train=False, transform=test_transform, download=True)
         test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-        class_mapping = dict(zip(COCO10_CLASSES, KOR_COCO10_CLSSES))
+        classes = KOR_COCO10_CLSSES
 
     elif dataset_name.lower() == "cifar100":
         test_transform = transforms.Compose([
@@ -65,7 +78,7 @@ def load_data(dataset_name, batch_size=32, image_size=224, num_workers=2):
         ])
         test_data = torchvision.datasets.CIFAR100(root="./data", train=False, transform=test_transform, download=True)
         test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-        class_mapping = dict(zip(COCO100_CLASSES, KOR_COCO100_CLASSES))
+        classes = KOR_COCO100_CLASSES
 
     elif dataset_name.lower() == "imagenet":
         try:
@@ -81,12 +94,34 @@ def load_data(dataset_name, batch_size=32, image_size=224, num_workers=2):
             transforms.ToTensor(),
             transforms.Normalize([0.48145466, 0.4578275, 0.40821073], [0.26862954, 0.26130258, 0.27577711])
         ])
-        test_
         images = ImageNetV2Dataset(transform=test_transform, location="./data")
         test_loader = DataLoader(images, batch_size=batch_size, num_workers=num_workers)
-        class_mapping = dict(zip(COCO100_CLASSES, KOR_COCO100_CLASSES))
+        classes = KOR_IMAGENET_CLASSES
 
-    return test_data, test_loader, class_mapping
+    return test_loader, classes 
+
+
+def get_text_features(model, tokenizer, classes, device):
+    with torch.no_grad():
+        text_features = []
+        for classname in tqdm(classes):
+            texts = [template.format(classname) for template in templates] #format with class
+            text_inputs = tokenizer(
+                texts,
+                return_tensors="pt",
+                padding=True
+            ).to(device)
+
+            class_embeddings = model.get_text_features(**text_inputs)
+            class_embeddings /= class_embeddings.norm(dim=-1, keepdim=True)
+
+            class_embedding = class_embeddings.mean(dim=0)
+            class_embedding /= class_embedding.norm()
+
+            text_features.append(class_embedding)
+
+        text_features = torch.stack(text_features, dim=1).to(device)
+    return text_features 
 
 
 def main(args):
@@ -95,19 +130,15 @@ def main(args):
     tokenizer = AutoTokenizer.from_pretrained(args.model)
     model = AutoModel.from_pretrained(args.model).to(device)
 
-    test_data, test_loader, class_mapping = load_data(
+    test_loader, classes = load_data(
         args.dataset, 
         args.batch_size,
         model.config.vision_config.image_size
     )
 
-    text_inputs = tokenizer(
-        [f"{class_mapping[c]}의 사진" for c in test_data.classes],
-        return_tensors="pt",
-        padding=True
-    ).to(device)
+    text_features = get_text_features(model, tokenizer, classes, device)
 
-    eval(model, text_inputs, test_loader, device)
+    evaluation(model, text_features, test_loader, device)
 
 
 if __name__ == "__main__":
